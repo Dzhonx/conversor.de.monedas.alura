cd960aee091756dc7723d33b6b702154b8e74aff
Jhon Ramirez Ledesma <jhonrale1402@gmail.com>
2025-11-14 09:56:25 -0500

    primer commit

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+    public static void main(String[] args) {
+        /*
+        * Sea bienvenido/a al Conversor de Moneda ]
+        1) Dólar >> Peso argentino
+        2) Peso argentino >> Dólar
+        3) Dólar >> Real brasileño
+        4) Real brasileño >> Dólar
+        5) Dólar >>> Peso colombiano
+        6) Peso colombiano >> Dólar
+        7) Salir
+        Elija una opción välida:
+        * */
+    }
+}
