198ca401002a770c049321b8fcedd1b2621728a8
Jhon Ramirez Ledesma <jhonrale1402@gmail.com>
2025-11-17 22:24:13 -0500

    Actualizando aplicacion

--- src/ConsultaTasa.java
+++ src/ConsultaTasa.java
@@ -11,13 +11,13 @@ public class ConsultaTasa {
     //Mi clave generada por la API de conversion
     private static final String claveApi = "0227dc08227b0a97a9346152";
 
+    //Creamos el metodo consultarPar para convertir las monedas
     public ResultadoConversion consultarPar(String monedaOrigen, String monedaDestino, double monto) {
 
-        String url = String.format(
-                "https://v6.exchangerate-api.com/v6/%s/pair/%s/%s",
-                claveApi, monedaOrigen, monedaDestino
-        );
+        //Construimos la url para consultar a la API, usamos el metodo format que evita errores y mantiene mas ordenado la url
+        String url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s", claveApi, monedaOrigen, monedaDestino);
 
+        //Preparamos y enviamos la peticion web que nos permitira obtener la tasa de conversion
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest req = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -25,21 +25,29 @@ public class ConsultaTasa {
                 .build();
 
         try {
+            //client.send() -> envia la peticion
+            //BodyHandlers.ofString() -> solicita la respuesta en string
             HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
 
+            //200 = OK -> pero si lanza otro codigo significa que algo salio mal
             if (res.statusCode() != 200)
                 throw new RuntimeException("Error API: código " + res.statusCode());
 
+            //res.body() -> trae el contenido en string
+            //JsonParser.parseString() -> convierte la respuesta en json , el cual ya podemos leer
             JsonObject json = JsonParser.parseString(res.body()).getAsJsonObject();
 
+            //Algunas APIs  responden con error aunque el codigo sea 200, este if detecta este tipo de errores
             if (json.has("result") && json.get("result").getAsString().equals("error")) {
                 throw new RuntimeException("API: " + json.get("error-type").getAsString());
             }
 
+            //Obteniendo tasa de conversion, se extrae del json
             double tasa = json.get("conversion_rate").getAsDouble();
+            //Calculo del monto convertido
             double resultado = tasa * monto;
 
-            //Objeto como resultado de la conversion
+            //Objeto como resultado de la conversion, el cual se envia a la clase Principal para su impresion
             return new ResultadoConversion(
                     monedaOrigen,
                     monedaDestino,
@@ -47,6 +55,7 @@ public class ConsultaTasa {
                     resultado
             );
 
+            //Si algo falla en el try , se envia al catch
         } catch (Exception e) {
             throw new RuntimeException("No se pudo consultar la API: " + e.getMessage());
         }
--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
+        //Leemos lo que el usuario escribe
         Scanner scanner = new Scanner(System.in);
+        //Instanciamos y creamos un objeto de tipo ConsultaTasa
         ConsultaTasa consulta = new ConsultaTasa();
         int opcion = 0;
 
@@ -22,21 +24,26 @@ public class Principal {
             System.out.print("Seleccione una opción: ");
 
             try {
+                //Convierte lo que se scaneo en un numero
                 opcion = Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
+                //En caso no escriba un numero
                 System.out.println("Ingrese un número válido.\n");
                 continue;
             }
 
+            //Esto cierra el programa
             if (opcion == 7) {
                 System.out.println("¡Gracias por usar el conversor!");
                 break;
             }
 
+            //Se crean las variables que guardaran las monedas a usar
             String origen = "";
             String destino = "";
 
             switch (opcion) {
+                //Se asigna la moneda a las variables
                 case 1 -> { origen = "USD"; destino = "ARS"; }
                 case 2 -> { origen = "ARS"; destino = "USD"; }
                 case 3 -> { origen = "USD"; destino = "BRL"; }
@@ -49,17 +56,24 @@ public class Principal {
                 }
             }
 
+            //Se solicita el monto
             System.out.print("Ingrese el monto a convertir: ");
             double monto;
 
             try {
+                //Se convierte lo scaneado en numero
                 monto = Double.parseDouble(scanner.nextLine());
             } catch (NumberFormatException e) {
+                //Error controlado si el usuario escribe una letra
                 System.out.println("Monto inválido.\n");
                 continue;
             }
 
             try {
+                /*
+                * El metodo consultaPar() retorna un resultado de tipo ResultadoConversion
+                * el cual se la asignamos a la variable r
+                * */
                 ResultadoConversion r = consulta.consultarPar(origen, destino, monto);
                 System.out.println("\n--- RESULTADO ---");
                 System.out.println("Moneda base: " + r.codigoMonedaOrigen());
